3b058c89eecc04a5f82b11f5aecd191d820568b9
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2026-03-17 23:01:28 +0100

    feat(domain): add Conference model, repository interface, and domain errors

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrUserExists             = errors.New("user already exists")
 	ErrDatabaseInit           = errors.New("failed to initialize database")
 	ErrRefreshSessionNotFound = errors.New("refresh session not found")
+	ErrConferenceNotFound     = errors.New("conference not found")
 )
